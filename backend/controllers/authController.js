1c0764f1f7a5383ab1024916fef7ab571fdd58af
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 14:19:44 -0500

    Complete login user with email and password validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -26,3 +26,37 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       token
    })
 })
+
+/*======================================================
+   login user -> /api/v1/login
+======================================================*/
+exports.loginUser = catchAsyncErrors( async (req, res, next) => {
+	const { email, password } = req.body;
+
+	//**************** check if email and password is entered by user****************//
+	if (!email || !password) {
+		return next(new ErrorHandler('Please enter email & password', 400));
+	}
+
+	//**************** finding user in database ****************//
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user) {
+		return next(new ErrorHandler('Invalid Email or Password', 401));
+	}
+
+	//**************** checks if password is correct or not ****************//
+	const isPasswordMatched = await user.comparePassword(password);
+
+	if (!isPasswordMatched) {
+		return next(new ErrorHandler('Invalid Email or Password', 401));
+	}
+
+	const token = user.getJwtToken();
+
+	res.status(200).json({
+		success: true,
+		token: token
+	})
+	// sendToken(user, 200, res);
+})
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 //**************** imports ****************//
 const express = require('express');
-const { registerUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 
 //**************** variables ****************//
 const router = express.Router();
 
 //**************** auth routes ****************//
 router.route('/register').post(registerUser);
-// router.route('/login').post(loginUser);
+router.route('/login').post(loginUser);
 
 module.exports = router;
