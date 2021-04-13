42f6c14b09588e42ef0e70b3daf526da5adcce62
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 18:31:41 -0500

    Complete sending jwt token in cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@
 const User = require('../models/user');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-
+const sendToken = require('../utils/jwtToken')
 /*===================================================
    register a new user -> /api/v1/register
 ======================================================*/
@@ -19,12 +19,10 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 		},
 	});
 
-	const token = user.getJwtToken();
+	
 
-   res.status(201).json({
-      success: true,
-      token
-   })
+   sendToken(user, 200, res);
+
 })
 
 /*======================================================
@@ -52,11 +50,6 @@ exports.loginUser = catchAsyncErrors( async (req, res, next) => {
 		return next(new ErrorHandler('Invalid Email or Password', 401));
 	}
 
-	const token = user.getJwtToken();
-
-	res.status(200).json({
-		success: true,
-		token: token
-	})
-	// sendToken(user, 200, res);
+	sendToken(user, 200, res);
+	
 })
