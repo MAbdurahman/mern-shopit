48c47048fde0f60af6d67827d82463c85a223836
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 12:15:23 -0500

    Complete pagination function for apiFeatures.js and productControllers.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,12 +18,15 @@ exports.newProduct = catchAsyncErrors( async (req, res, next) => {
    get all products -> /api/v1/products?keyword=apple
 ============================================================*/
 exports.getProducts = catchAsyncErrors( async (req, res, next) => {
-	const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter();
+	const resPerPage = 4;
+	const productsCount = await Product.countDocuments();
+	const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter().pagination(resPerPage);
 	const products = await apiFeatures.query;
 
 	res.status(200).json({
 		success: true,
 		count: products.length,
+		productsCount,
 		products,
 	});
 });
