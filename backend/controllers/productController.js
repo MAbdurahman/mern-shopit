fd0c12792c0db846a74729354e92a67e91d480a7
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 11:36:48 -0500

    Add search method to productController.js and product.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@
 const Product = require('../models/product');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 /*===================================================
 creates new product -> /api/v1/admin/product/new
 ======================================================*/
@@ -13,11 +14,13 @@ exports.newProduct = catchAsyncErrors( async (req, res, next) => {
 	});
 });
 
-/*===================================================
+/*=========================================================
    get all products -> /api/v1/products?keyword=apple
-======================================================*/
+============================================================*/
 exports.getProducts = catchAsyncErrors( async (req, res, next) => {
-	const products = await Product.find();
+	const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+	const products = await apiFeatures.query;
+
 	res.status(200).json({
 		success: true,
 		count: products.length,
