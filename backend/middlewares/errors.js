eb45cb2fca19e6f3a39215cb4a4842bd511368b1
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 07:40:11 -0500

    Complete handling Validation and Mongoose Errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -22,11 +22,13 @@ module.exports = (err, req, res, next) => {
 		if (err.name === 'CastError') {
 			const message = `Resource not found! Invalid: ${err.path}`;
 			error = new ErrorHandler(message, 400);
+			
 		}
 		//**************** handling mongoose validation error ****************//
 		if (err.name === 'ValidationError') {
 			const message = Object.values(err.errors).map(value => value.message);
 			error = new ErrorHandler(message, 400);
+
 		}
 
 		res.status(error.statusCode).json({
