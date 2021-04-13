e8490d960e303a7b07247767cb91acec6e820529
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 07:23:00 -0500

    Complete Unhandled Promise Rejection

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
 
-
-
 //**************** configuration setup ****************//
 dotenv.config({path: 'backend/config/config.env'});
 
@@ -14,10 +12,17 @@ const NODE_ENV = process.env.NODE_ENV;
 
 //**************** connect to database ****************//
 connectDatabase();
-
 //**************** app listening ****************//
 const server = app.listen(PORT, () => {
    console.log(
 		`The server is listening at - http://localhost:${PORT} in ${NODE_ENV} mode🔥`
 	);
+});
+//**************** handling unhandled promise rejection ****************//
+process.on('unhandledRejection', err => {
+	console.log(`ERROR: ${err.stack}`);
+	console.log('Shutting down process due to Unhandled Promise rejection!');
+	server.close(() => {
+		process.exit(1);
+	});
 });
