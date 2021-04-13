a4e6814eea4763c8d84abae429f68ba885ab1aa9
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-13 12:00:49 -0500

    Add filter function to apiFeatures.js and productController.js

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -26,7 +26,7 @@ class APIFeatures {
 		const removeFields = ['keyword', 'limit', 'page'];
 		removeFields.forEach(el => delete queryCopy[el]);
 
-		// Advance filter for price, ratings etc
+		// Advance filter for price, ratings, and etcetera
 		let queryStr = JSON.stringify(queryCopy);
 		queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
 
