49a9380b83740c001d64b3cdbcf932365d222f54
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-14 12:36:21 -0500

    Complete update and delete user by their ID for admin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -248,3 +248,47 @@ exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
 		user,
 	});
 });
+
+/*============================================================
+   (admin) update user profile -> /api/v1/admin/user/:id
+===============================================================*/
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+	const newUserData = {
+		name: req.body.name,
+		email: req.body.email,
+		role: req.body.role,
+	};
+
+	const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+		new: true,
+		runValidators: true,
+		useFindAndModify: false,
+	});
+
+	res.status(200).json({
+		success: true,
+	});
+});
+
+/*============================================================
+   (admin) delete user -> /api/v1/admin/user/:id
+===============================================================*/
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+	const user = await User.findById(req.params.id);
+
+	if (!user) {
+		return next(
+			new ErrorHandler(`User is not found with id: ${req.params.id}`)
+		);
+	}
+
+	// Remove avatar from cloudinary
+	/*  const image_id = user.avatar.public_id;
+    await cloudinary.v2.uploader.destroy(image_id); */
+
+	await user.remove();
+
+	res.status(200).json({
+		success: true,
+	});
+});
