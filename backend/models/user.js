57edd9a9afe1b82425a50308172e919263bc1847
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-14 17:57:43 -0500

    Create the orderSchema in order.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -82,4 +82,4 @@ userSchema.methods.getResetPasswordToken = function () {
 	return resetToken;
 };
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('User', userSchema);
