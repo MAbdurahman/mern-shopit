210345a70f1e53dcbc59e65c22e95ccd2556073b
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-14 05:55:13 -0500

    Add send password recovery email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 //**************** imports ****************//
 const express = require('express');
-const { registerUser, loginUser, logout } = require('../controllers/authController');
+const { registerUser, loginUser, logout, forgotPassword } = require('../controllers/authController');
 
 //**************** variables ****************//
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logout);
+router.route('/password/forgot').post(forgotPassword);
 
 module.exports = router;
