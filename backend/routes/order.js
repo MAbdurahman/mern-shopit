a18872bccf71eddb0eec3f9d80a0d49db0788e37
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-15 04:29:46 -0500

    Complete get all orders for admin

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,7 +3,8 @@ const express = require('express');
 const {
    newOrder,
    getSingleOrder,
-   myOrders
+   myOrders,
+   allOrders
 } = require('../controllers/orderController');
 const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
@@ -14,6 +15,7 @@ const router = express.Router();
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/orders/me').get(isAuthenticatedUser, myOrders);
+router.route('/admin/orders/').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
 
 
 
