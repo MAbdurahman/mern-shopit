723f6c9b921e900054759ec90f191f8892746c87
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-15 19:11:50 -0500

    Add components Header.jsx and Footer.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div>
-      <h1>ShopIT</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+   return (
+		<>
+			<footer className='py-1'>
+				<p className='text-center mt-1'>
+					&copy;&nbsp;2021 Shopping Cart, All Rights Reserved
+				</p>
+			</footer>
+		</>
+	);
+}
