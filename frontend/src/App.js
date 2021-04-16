6cf471d6fd95508d454f329176eaaa8076b6d524
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-16 04:13:35 -0500

    Create static Home.jsx and add to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
       <Footer />
     </div>
   );
