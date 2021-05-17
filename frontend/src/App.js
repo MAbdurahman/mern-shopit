55d01645b3a85475553d270ddffe17a52263371c
MAbdurahman <mdbdrrhm@gmail.com>
2021-05-17 15:43:33 -0500

    Add Shipping.jsx, CheckoutSteps.jsx, and ConfirmOrder.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './components/Home';
 import ProductDetails from './components/product/ProductDetails';
+
 import Login from './components/user/Login';
 import Register from './components/user/Register';
 import Profile from './components/user/Profile';
@@ -11,10 +13,12 @@ import UpdateProfile from './components/user/UpdateProfile';
 import UpdatePassword from './components/user/UpdatePassword';
 import ForgotPassword from './components/user/ForgotPassword';
 import NewPassword from './components/user/NewPassword';
-import ProtectedRoute from './components/route/ProtectedRoute';
 
 import Cart from './components/cart/Cart';
+import Shipping from './components/cart/Shipping';
+import ConfirmOrder from './components/cart/ConfirmOrder';
 
+import ProtectedRoute from './components/route/ProtectedRoute';
 import { loadUser } from './actions/userActions';
 import store from './store';
 // import axios from 'axios';
@@ -36,6 +40,8 @@ function App() {
 					<Route exact path='/product/:id' component={ProductDetails} />
 
 					<Route path='/cart' component={Cart} exact />
+					<ProtectedRoute path='/shipping' component={Shipping} />
+					<ProtectedRoute path='/confirm' component={ConfirmOrder} exact />
 
 					<Route path='/login' component={Login} />
 					<Route path='/register' component={Register} />
